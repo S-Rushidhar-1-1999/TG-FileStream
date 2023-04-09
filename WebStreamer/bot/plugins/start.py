bdc876d993bdc1c2cdcefb261beea3a2324a6b3c
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-04-09 19:07:24 +0530

    Add files via upload

---
+++ WebStreamer/bot/plugins/start.py
@@ -0,0 +1,56 @@
+# This file is a part of TG-FileStreamBot
+# Coding : Jyothis Jayanth [@EverythingSuckz]
+
+from pyrogram import filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+from WebStreamer.vars import Var 
+from WebStreamer.bot import StreamBot
+from pyrogram.types import Message
+
+@StreamBot.on_message(filters.command("start") & filters.private)
+async def start(b, m: Message):
+    if Var.UPDATES_CHANNEL != "None":
+        try:
+            user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+            if user.status == enums.ChatMemberStatus.BANNED:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="__Sorry, you are banned. Contact My Owner [ Rushidhar ](https://telegram.me/Rushidhar1999)__",
+                    disable_web_page_preview=True
+                )
+                return
+        except UserNotParticipant:
+             await StreamBot.send_photo(
+                chat_id=m.chat.id,
+                photo="https://te.legra.ph/file/95db9bf6f91bd96d7a9f1.jpg",
+                caption="<i>🔐 Join Channel To Use Me 🔐</i>",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🔓 Join Now 🔓", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                        ]
+                    ]
+                ),
+                
+            )
+             return
+        except Exception:
+            await b.send_message(
+                chat_id=m.chat.id,
+                text="<i>Something went wrong</i> <b> <a href='https://telegram.me/Rushidhar1999'>CLICK HERE FOR SUPPORT </a></b>",
+
+                disable_web_page_preview=True)
+            return
+
+    await StreamBot.send_photo(
+        chat_id=m.chat.id,
+        photo ="https://te.legra.ph/file/95db9bf6f91bd96d7a9f1.jpg",
+        caption =f'Hi {m.from_user.mention(style="md")}!,\n\nI am Telegram File to Link Generator Bot.\n\nSend me any file and get a direct download link and streamable link.!',
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("👨‍💻 Owner 👨‍💻", url="https://telegram.me/Rushidhar1999")]
+            ]
+        )
+    )
+    
---
+++ WebStreamer/bot/plugins/stream.py
@@ -0,0 +1,185 @@
+# This file is a part of TG-FileStreamBot
+# Coding : Jyothis Jayanth [@EverythingSuckz]
+
+import logging
+from pyrogram import filters, errors, Client, enums
+from pyrogram.errors import FloodWait, UserNotParticipant
+from WebStreamer.vars import Var
+from urllib.parse import quote_plus
+from WebStreamer.bot import StreamBot, logger
+from WebStreamer.utils import get_hash, get_name, get_media_file_size
+from WebStreamer.utils.human_readable import humanbytes
+from pyrogram.enums.parse_mode import ParseMode
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@StreamBot.on_message(
+    filters.private
+    & (
+        filters.document
+        | filters.video
+        | filters.audio
+        | filters.animation
+        | filters.voice
+        | filters.video_note
+        | filters.photo
+        | filters.sticker
+    ),
+    group=4,
+)
+async def media_receive_handler(c: Client, m: Message):
+    if Var.UPDATES_CHANNEL != "None":
+        try:
+            user = await c.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+            if user.status == enums.ChatMemberStatus.BANNED:
+                await c.send_message(
+                    chat_id=m.chat.id,
+                    text="__Sorry, you are banned. Contact My Owner [ Rushidhar ](https://telegram.me/Rushidhar1999)__",
+                    
+                    disable_web_page_preview=True
+                )
+                return 
+        except UserNotParticipant:
+            await c.send_message(
+                chat_id=m.chat.id,
+                text="""<i>🔐 Join Channel To Use Me 🔐</i>""",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🔓 Join Now 🔓", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                        ]
+                    ]
+                ),
+                
+            )
+            return
+        except Exception as e:
+            await m.reply_text(e)
+            await c.send_message(
+                chat_id=m.chat.id,
+                text="**Something Went Wrong. Contact** [ Rushidhar ](https://telegram.me/Rushidhar1999)",
+                
+                disable_web_page_preview=True)
+            return
+
+    try:
+        log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
+        file_hash = get_hash(log_msg, Var.HASH_LENGTH)
+        stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}"
+        short_link = f"{Var.URL}{file_hash}{log_msg.id}"
+        logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
+
+        msg_text = """<b><i>Your Link Generated !</i></b>\n\n<b>📂 File Name :</b> <i>{}</i>\n\n<b>🚸 Note : LINK WON'T EXPIRE TILL I DELETE</b>"""
+        await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True, quote=True)
+        await m.reply_text(
+            text=msg_text.format(quote_plus(get_name(m))),
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("😎 STREAM IN WEB/PLAYERS 😎", url=f"https://rushidhar1999.pythonanywhere.com/stream?url={stream_link}")],
+                    [InlineKeyboardButton("📥 DOWNLOAD 📥", url=stream_link)],
+                    [InlineKeyboardButton("❤️ SOURCE CODE ❤️", url="https://github.com/EverythingSuckz/TG-FileStreamBot")]
+                ]
+            )
+        )
+    except errors.ButtonUrlInvalid:
+        log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
+        file_hash = get_hash(log_msg, Var.HASH_LENGTH)
+        stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}"
+        short_link = f"{Var.URL}{file_hash}{log_msg.id}"
+        logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
+
+        await m.reply_text(
+            text="<code>{}</code>\n\nShortened: {})".format(
+                stream_link, short_link
+            ),
+            quote=True,
+            parse_mode=ParseMode.HTML,
+        )
+    except FloodWait as e:
+        print(f"Sleeping for {str(e.x)}s")
+        await asyncio.sleep(e.x)
+        await c.send_message(chat_id=Var.BIN_CHANNEL, text=f"Got Floodwait Of {str(e.x)}s from [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n\n**User Id :** `{str(m.from_user.id)}`", disable_web_page_preview=True)
+
+@StreamBot.on_message(filters.command(["link"]) & filters.chat(Var.AUTH_CHAT))
+async def group_media_receive_handler(c: Client, m: Message):
+    if Var.UPDATES_CHANNEL != "None":
+        try:
+            user = await c.get_chat_member(Var.UPDATES_CHANNEL, m.from_user.id)
+            if user.status == enums.ChatMemberStatus.BANNED:
+                await c.send_message(
+                    chat_id=m.chat.id,
+                    text="__Sorry, you are banned. Contact My Owner [ Rushidhar ](https://telegram.me/Rushidhar1999)__",
+                    
+                    disable_web_page_preview=True
+                )
+                return 
+        except UserNotParticipant:
+            await c.send_message(
+                chat_id=m.chat.id,
+                text="""<i>Join Channel To Use Me🔐</i>""",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("🔓 Join Now 🔓", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                        ]
+                    ]
+                ),
+                
+            )
+            return
+        except Exception as e:
+            await m.reply_text(e)
+            await c.send_message(
+                chat_id=m.chat.id,
+                text="**Something Went Wrong. Contact** [ Rushidhar ](https://telegram.me/Rushidhar1999)",
+                
+                disable_web_page_preview=True)
+            return
+    try:
+        msg = m.reply_to_message
+        if msg == None or not msg.video:
+            return await m.reply_text("Reply To a Valid Video")
+    except Exception as e:
+        return await m.reply_text(f"Error: {e}")
+    try:
+        log_msg = await msg.forward(chat_id=Var.BIN_CHANNEL)
+        file_hash = get_hash(log_msg, Var.HASH_LENGTH)
+        stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(msg))}?hash={file_hash}"
+        short_link = f"{Var.URL}{file_hash}{log_msg.id}"
+        logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
+
+        msg_text ="""<b><i>Your Link Generated !</i></b>\n\n<b>📂 File Name :</b> <i>{}</i>\n\n<b>📦 File Size :</b> <i>{}</i>\n\n<b>🚸 Note : LINK WON'T EXPIRE TILL I DELETE</b>"""
+
+        await log_msg.reply_text(text=f"#NewFile \n\n**User Name :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**User Id :** `{m.from_user.id}`\n**Stream Link :** {stream_link}", disable_web_page_preview=True,  quote=True)
+        await m.reply_text(
+            text=msg_text.format(get_name(msg), humanbytes(get_media_file_size(msg))),
+            quote=True,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton("😎 STREAM IN WEB/PLAYERS 😎", url=f"https://rushidhar1999.pythonanywhere.com/stream?url={stream_link}")],
+                    [InlineKeyboardButton("📥 DOWNLOAD 📥", url=stream_link)],
+                    [InlineKeyboardButton("❤️ SOURCE CODE ❤️", url="https://github.com/EverythingSuckz/TG-FileStreamBot")]
+                ]
+            )
+        )
+    except errors.ButtonUrlInvalid:
+        log_msg = await msg.forward(chat_id=Var.BIN_CHANNEL)
+        file_hash = get_hash(log_msg, Var.HASH_LENGTH)
+        stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(msg))}?hash={file_hash}"
+        short_link = f"{Var.URL}{file_hash}{log_msg.id}"
+        logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
+
+        await m.reply_text(
+            text="<code>{}</code>\n\nShortened: {})".format(
+                stream_link, short_link
+            ),
+            quote=True,
+            parse_mode=ParseMode.HTML,
+        )
+    except FloodWait as e:
+        print(f"Sleeping for {str(e.x)}s")
+        await asyncio.sleep(e.x)
+        await c.send_message(chat_id=Var.BIN_CHANNEL, text=f"Got Floodwait Of {str(e.x)}s from [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n\n**User Id :** `{str(m.from_user.id)}`", disable_web_page_preview=True)
